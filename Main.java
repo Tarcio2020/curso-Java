b42322638a6f7a1e390f702c78d3d49bde88a8a1
Tarcio2020 <94722456+Tarcio2020@users.noreply.github.com>
2023-07-02 21:36:18 -0300

    add

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+	    //println quebra linha
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo");
+		System.out.println("Bom dia!");
+		
+		
+		int preco = 75;
+		
+		System.out.println(preco*50);
+
+	}
+}
